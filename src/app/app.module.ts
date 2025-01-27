a56b5a3166180896733dae8a804604746dca8efd
Miguel Garcia <miguel.garciabaeza@iesdoctorbalmis.com>
2025-01-27 14:32:53 +0100

    Formulario de suscripcion y registro funcionan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormularioComponent } from './formulario/formulario.component';
+import { RegistroComponent } from './registro/registro.component';
+import { SuscripcionComponent } from './suscripcion/suscripcion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormularioComponent
+    FormularioComponent,
+    RegistroComponent,
+    SuscripcionComponent
   ],
   imports: [
     BrowserModule,
--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
-  standalone: true,
-  imports: [],
+  // standalone: true,
+  // imports: [],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.scss'
 })
-export class RegistroComponent {
 
+export class RegistroComponent {
+  registroForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registroForm = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.registroForm.valid) {
+      console.log('Datos del formulario:', this.registroForm.value);
+    }
+  }
 }
