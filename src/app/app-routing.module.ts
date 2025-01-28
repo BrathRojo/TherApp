efc3de4db0acf962a3ee969b96b4f5009823c7e2
Brath Bolaño <brath.bolano@iesdoctorbalmis.com>
2025-01-28 11:24:08 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio/inicio.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
 const routes: Routes = [
-  { path: '', component: InicioComponent }
+  { path: '', component: InicioComponent },
+  { path: 'registro', component: RegistroComponent }
+  
 ];
 
 @NgModule({
