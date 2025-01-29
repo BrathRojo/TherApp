69899647beb74a8a6b0c383165299de5a299afb2
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-01-29 12:52:52 +0100

    nada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
 import { NotFoundComponent } from './componentes/not-found/not-found.component';
 import { PublicacionComponent } from './componentes/publicacion/publicacion.component';
 import { TerapeutasComponent } from './componentes/terapeutas/terapeutas.component';
-import { UsuarioComponent } from './componentes/usuario/usuario.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +21,6 @@ import { UsuarioComponent } from './componentes/usuario/usuario.component';
     NotFoundComponent,
     PublicacionComponent,
     TerapeutasComponent,
-    UsuarioComponent,
   ],
   imports: [
     HttpClientModule,
--- src/app/componentes/terapeutas/terapeutas.component.ts
+++ src/app/componentes/terapeutas/terapeutas.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { TerapeutaService } from '../../services/terapeuta.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-terapeutas',
   templateUrl: './terapeutas.component.html',
-  styleUrls: ['./terapeutas.component.scss']
+  styleUrl: './terapeutas.component.scss'
 })
-export class TerapeutasComponent implements OnInit {
-  terapeutas: any[] = [];
+export class TerapeutasComponent {
 
-  constructor(private terapeutaService: TerapeutaService) {}
-
-  ngOnInit(): void {
-    this.terapeutaService.getAllTerapeutas().subscribe(
-      (data) => {
-        console.log('Datos recibidos:', data);
-        this.terapeutas = data;
-      },
-      (error) => {
-        console.error('Error al obtener terapeutas:', error);
-      }
-    );
-  }
 }
