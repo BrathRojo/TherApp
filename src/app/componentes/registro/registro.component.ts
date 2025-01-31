4cb3c006613a806d301f677c6dc1ddf09e346a30
Miguel Garcia <miguel.garciabaeza@iesdoctorbalmis.com>
2025-01-31 12:34:30 +0100

    pequeños cambios en el registro para intentar ingresar en la BBDD

--- src/app/componentes/registro/registro.component.ts
+++ src/app/componentes/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from "../../services/usuario.service";
 
 @Component({
   selector: 'app-registro',
@@ -10,12 +11,32 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegistroComponent {
   registroForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private usuarioService: UsuarioService) {
     this.registroForm = this.fb.group({
       nombre: ['', [Validators.required, Validators.minLength(3)]],
+      nombreUsuario: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
+      clave: ['', [Validators.required, Validators.minLength(8)]],
+      fotoPerfil: ['', Validators.required],
+      dni: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+      fechaNacimiento: ['', Validators.required],
+      telefono: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(9)]],
+      ubicacion: ['', Validators.required],
+      biografica: ['', Validators.required],
     });
+  }
+
+  registrarUsuario() {
+    if (this.registroForm.valid) {
+      this.usuarioService.registrarUsuario(this.registroForm.value).subscribe(
+        response => {
+          console.log('Usuario registrado con éxito', response);
+        },
+        error => {
+          console.error('Error al registrar usuario', error);
+        }
+      );
+    }
   }
 
   onSubmit() {
