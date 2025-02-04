fa5d2784c9f826bbabdc443cdb20cccbb4c6278f
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-02-04 14:44:55 +0100

    cosas

--- src/app/componentes/carrusel/carrusel.component.ts
+++ src/app/componentes/carrusel/carrusel.component.ts
@@ -9,7 +9,7 @@ import { Card } from '../../interfaces/card';
 export class CarruselComponent {
 
   cards:Card[] = [
-    {foto:"assets/terapeuta1.jpg", titulo:"Nombre de persona", texto:"Datos específicos de la persona", precio:50, enlace:"/"}, 
+    {foto:"assets/terapeuta1.jpg", titulo:"Manolo Pérez", texto:"Psicólogo con más de 20 años de experiencia en clínicas especializadas en TCAs<br>Especialista en EMDR y mindfullness<br>Formación actualizada y constante ", precio:50, enlace:"/"}, 
     {foto:"assets/terapeuta2.jpg", titulo:"Nombre de persona 2", texto:"Datos de la persona 2", precio:200, enlace:"/"}, 
     {foto:"assets/terapeuta3.jpg", titulo:"Nombre de persona 3", texto:"Datos de la persona 3", precio:-1, enlace:"/"}];
 
