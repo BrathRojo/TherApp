1c45b97fa9b868f5ae6074e469a91c8f46085496
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-02-10 14:31:12 +0100

    falta sacar la foto y el nombre de usuario

--- src/app/componentes/carrusel/carrusel.component.ts
+++ src/app/componentes/carrusel/carrusel.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Card } from '../../interfaces/card';
 import { TerapeutaService } from '../../services/terapeuta.service';
 import { Terapeuta } from '../../interfaces/terapeuta';
+import { UsuarioService } from '../../services/usuario.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-carrusel',
@@ -14,34 +16,29 @@ export class CarruselComponent implements OnInit{
 
   cards:Card[] = [];
 
-  constructor(private servicio: TerapeutaService){}
+  constructor(private servicio: TerapeutaService, private servicio2: UsuarioService){}
   ngOnInit():void{
     
     this.servicio.getAllTerapeutas().subscribe({
       next: (datos) => {
-        this.terapeutas = [...datos]
+        this.terapeutas = [...datos];
+
+        this.cards = this.terapeutas.map(t =>({
+          foto: t.fotoPerfil,
+          titulo: t.nombre,
+          texto: t.especialidad,
+          precio: t.precio,
+          enlace: "/"
+        }));
+
+        console.log("Cards generadas:", this.cards);
       },
       error: (error) => {
         console.log('error', error);
         this.terapeutas = [];
       }
     });
-
-    for (let index = 0; index < this.terapeutas.length; index++) {
-        this.cards.push({foto:this.terapeutas[index].fotoPerfil, titulo:this.terapeutas[index].nombre, texto:this.terapeutas[index].biografica,precio:this.terapeutas[index].precio, enlace:"/"});
-      
-    }
   }
-
-  // cards:Card[] = [
-  //   {foto:"assets/terapeuta1.jpg", titulo:this.terapeutas[0].nombre, texto:"Psicólogo con más de 20 años de experiencia en clínicas especializadas en TCAs<br>Especialista en EMDR y mindfullness<br>Formación actualizada y constante ", precio:50, enlace:"/"}, 
-  //   {foto:"assets/terapeuta2.jpg", titulo:"Nombre de persona 2", texto:"Datos de la persona 2", precio:200, enlace:"/"}, 
-  //   {foto:"assets/terapeuta3.jpg", titulo:"Nombre de persona 3", texto:"Datos de la persona 3", precio:-1, enlace:"/"}];
-
-  
-
-  
-
 
     @Input() datos: Card[] = [];
 
