bdb195a29c05291848aa6431f53ca71609bda9d9
Brath Bolaño <brath.bolano@iesdoctorbalmis.com>
2025-02-11 14:44:42 +0100

    cosa de rama que pongo aqui para no tocar lo del WebScoket

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  
+  logged: boolean = false;
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.auth.isLoggedIn().subscribe(loggedIn => {
+      this.logged = loggedIn;
+    });
+  }
+
+  redirectHome(): void {
+    if (this.logged) {
+      this.router.navigate(['/home']);
+    }
+    else {
+      this.router.navigate(['/']);
+    }
+  }
+
+  cerrarSesion(): void {
+    this.auth.logout();
+    this.router.navigate(['/']);
+  }
 }
