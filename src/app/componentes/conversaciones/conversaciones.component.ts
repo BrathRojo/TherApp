cb3d33fcb14f092afa726cbce88e9f49e651b8f3
Brath Bolaño <brath.bolano@iesdoctorbalmis.com>
2025-02-12 08:28:56 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ src/app/componentes/conversaciones/conversaciones.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conversaciones',
+  templateUrl: './conversaciones.component.html',
+  styleUrl: './conversaciones.component.scss'
+})
+export class ConversacionesComponent {
+
+}
