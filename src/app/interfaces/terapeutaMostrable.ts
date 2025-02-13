e306a7cff04af931eb0a4bdb41534789347ee97c
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-02-13 10:37:20 +0100

    carrusel arreglado

---
+++ src/app/interfaces/terapeutaMostrable.ts
@@ -0,0 +1,9 @@
+export interface terapeutaMostrable{
+    nombre: string;
+    apellidos: string;
+    foto: string;
+    experiencia: string;
+    especialidad: string;
+    precio: number;
+    premium: boolean;
+}
