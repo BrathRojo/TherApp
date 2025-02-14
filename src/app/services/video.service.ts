add65fffda5129909188d41c05d2ff790f257413
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-02-14 12:58:06 +0100

    publicaciones a medias

--- src/app/services/video.service.ts
+++ src/app/services/video.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Multimedia } from '../interfaces/multimedia';
+import { Publicacion } from '../interfaces/publicacion';
 
 @Injectable({
   providedIn: 'root'
