0567c5b2e27cc77f09b54b74775190a11eb9cfe1
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-02-17 11:26:04 +0100

    cosas de zona admin

--- src/app/componentes/zonaadmin/zonaadmin.component.ts
+++ src/app/componentes/zonaadmin/zonaadmin.component.ts
@@ -6,13 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './zonaadmin.component.scss'
 })
 export class ZonaadminComponent {
-<<<<<<< HEAD
   accordionItems = [
     { id: 1, title: 'Accordion Item #1', content: 'Este es el contenido del primer ítem.' },
     { id: 2, title: 'Accordion Item #2', content: 'Este es el contenido del segundo ítem.' },
     { id: 3, title: 'Accordion Item #3', content: 'Este es el contenido del tercer ítem.' }
   ];
-=======
-
->>>>>>> 37de261 (cosas)
 }
