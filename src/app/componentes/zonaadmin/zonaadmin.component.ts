37de26190a027f55efbbaccaea2c5877d415ecd6
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-02-17 10:18:05 +0100

    cosas

---
+++ src/app/componentes/zonaadmin/zonaadmin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zonaadmin',
+  templateUrl: './zonaadmin.component.html',
+  styleUrl: './zonaadmin.component.scss'
+})
+export class ZonaadminComponent {
+
+}
