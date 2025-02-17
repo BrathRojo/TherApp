0b5a721dc6728b1f267befe5229791a6c348447d
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-02-17 13:30:35 +0100

    avances en la zona admin

--- src/app/componentes/zonaadmin/zonaadmin.component.ts
+++ src/app/componentes/zonaadmin/zonaadmin.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TerapeutaService } from '../../services/terapeuta.service';
+import { terapeutaMostrable } from '../../interfaces/terapeutaMostrable';
 
 @Component({
   selector: 'app-zonaadmin',
   templateUrl: './zonaadmin.component.html',
   styleUrl: './zonaadmin.component.scss'
 })
-export class ZonaadminComponent {
+export class ZonaadminComponent implements OnInit{
+
+  constructor(private servicio: TerapeutaService) { }
+
+  @Input() terapeutas: Array<{ nombre: string, apellidos: string, premium: boolean }> = [];
+
+  ngOnInit(): void {
+      this.servicio.getTerapeutasParaMostrar().subscribe({
+        next: (terapeutas) => {
+          this.terapeutas = terapeutas.map(t => ({
+            nombre: t.nombre,
+            apellidos: t.apellidos,
+            premium: t.premium
+          }));
+        },
+        error: (err) => console.error('Error al obtener terapeutas:', err)
+      });
+  }
+
   accordionItems = [
     { id: 1, title: 'Accordion Item #1', content: 'Este es el contenido del primer ítem.' },
     { id: 2, title: 'Accordion Item #2', content: 'Este es el contenido del segundo ítem.' },
