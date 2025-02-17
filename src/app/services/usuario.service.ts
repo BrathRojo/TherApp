e95dbb04d24064ecffac2e7f81fa184f0628b926
David Ramirez <david.ramirez@iesdoctorbalmis.com>
2025-02-17 15:02:44 +0100

    retoques

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -43,7 +43,7 @@ export class UsuarioService {
     return this.http.get<Usuario[]>(`${this.apiUrl}/mas-en-therapp?usuarioId=${usuarioId}`);
   }
 
-  hacerAdmin(email: String): Observable<any>{
-    return this.http.post<any>(`${this.apiUrl}/haceradmin`, email);
-  }
+  hacerAdmin(email: String): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/haceradmin`, { email });
+}
 }
